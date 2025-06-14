0ac6f871697e6f949ea30ee622b1058520d203a1
NehaDwivedi842 <109571688+NehaDwivedi842@users.noreply.github.com>
2025-06-14 15:03:22 +0530

    Update Assignment_16_8.py

--- Assignment_16/Assignment_16_8.py
+++ Assignment_16/Assignment_16_8.py
@@ -1,7 +1,7 @@
 def remove_blank_lines(input_file, output_file):
     with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
         for line in infile:
-            if line.strip():  # True if line is not empty
+            if line.strip(): 
                 outfile.write(line)
 
 remove_blank_lines("marks.txt", "cleaned_marks.txt")
